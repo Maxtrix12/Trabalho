b974329b77cd49624aab4be539901555440a5b54
Maxtrix12 <hypercar.matrix@gmail.com>
2024-10-02 15:40:29 -0300

    first commit

--- PGlogin.js
+++ PGlogin.js
@@ -1,3 +1,43 @@
 function handleLogin() {
     location.reload(); 
+    
+
 }
+
+function logar() {
+
+ var login = document.getElementById('usuario').value;
+ var senha =document.getElementById('senha').value;
+
+ if(login =="Marcos2424@gmail.com" && senha == "Marcosamatraveco"){
+
+    alert('Sucesso');
+    location.href = "Gerente.html"
+ }else{
+    alert('Usuario ou senha incorreta')
+ }
+ 
+}
+
+function logar() {
+
+   var login = document.getElementById('usuario').value;
+   var senha =document.getElementById('senha').value;
+  
+   if(login =="yago" && senha == "matos"){
+  
+      alert('Sucesso');
+      location.href = "Vendedor.html"
+   }else{
+      alert('Usuario ou senha incorreta')
+   }
+   
+  }
+  
+
+   
+
+
+  
+
+
